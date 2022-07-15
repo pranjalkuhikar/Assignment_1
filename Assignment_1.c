9268d3d0496430f701f011bce2a631c14d29dcc4
Pranjal <pranjalkuhikar123@gmail.com>
2022-07-15 20:07:00 +0530

    Assignment1

---
+++ Assignment_1.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+
+{
+    // // 1]Write a program that print your name,address,city,pin code ?
+    // printf("Name:- Pranjal Abhimanyu Kuhikar\n");
+    // printf("Address:- Panchpaoli Nandgiri Road Ladpura Nagpur\n");
+    // printf("City:- Nagpur\n");
+    // printf("Pin code:- 440017\n");
+    // return 0;
+
+    // // 2]Write a program that print sum of two numbers 10 and 20 ?
+    // int a = 10;
+    // int b = 20;
+    // int sum = a + b;
+    // printf("The sum of two number is %d\n", sum);
+    // return 0;
+
+    // // 3]Write a program that calculate and print the sum,average,multiplication,absolute value of three numbers. i.e. 20,50.2,20.3?
+    // int c = 20;
+    // float d = 50.2;
+    // float e = 20.3;
+    // printf("Sum = %.1f\n", c + d + e);
+    // printf("Average = %.1f\n", (c + d + e) / 3);
+    // printf("Multiplication = %.1f\n", c * d * e);
+    // printf("Absolute value of three number = %.1f\n", c + d + e);
+    // return 0;
+
+    // // 4]Write a program that converts the 50`C temperature to `F ?
+    // int celsius = 50;
+    // printf("The Faranite value is %d\n", (celsius * 9 / 5) + 32);
+    // return 0;
+
+    // // 5]Write a program that convert the distance 55km into m,cm,mm ?
+    // int distance = 55;
+    // printf("km to m = %d\n", distance * 1000);
+    // printf("km to cm = %d\n", distance * 100000);
+    // printf("km to mm = %d\n", distance * 1000000);
+    // return 0;
+
+    // // 6]Write a program that convert the weight 5mg into g,kg ?
+    // float weight = 5;
+    // printf("mg to g = %.3f\n", weight / 1000);
+    // printf("mg to kg = %f\n", weight / 1000000);
+    // return 0;
+
+    // 7]Write a programms that solve the given expressions:
+    // x=2, y=3.5, r=1.5, h=10
+    int x = 2;
+    float y = 3.5;
+    float r = 1.5;
+    int h = 10;
+    // [a]
+    printf("x+y * x/y = %.1f\n", x + y * x / y);
+    // [b]
+    printf("p = %.1f\n", pow(x, 3) - pow(y, 3));
+    // [c]
+    printf("q = %.1f\n", x * pow(y, 2));
+    // [d]
+    printf("t = %.1f\n", pow(x, 2) - pow(y, 2) + 2 * x * y);
+    // [e]
+    printf("s =  %.1f\n", y * ((pow(x, 2)) - 1));
+    // [f]
+    printf("a = %.1f\n", 2 * 3.24 * r);
+    // [g]
+    printf("z = %.1f", 4 / 3 * 3.14 * r * r * h);
+    return 0;
+}
